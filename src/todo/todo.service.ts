78b73bdd54ae7e047a7d737287edafec4febc5a2
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-10 21:29:13 -0700

    services

---
+++ src/todo/todo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { Todo } from './entity/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [
+    { id: 1, description: 'Tarea 1', done: false },
+    { id: 2, description: 'Tarea 2', done: false },
+    { id: 3, description: 'Tarea 3', done: false },
+    { id: 4, description: 'Tarea 4', done: false },
+    { id: 5, description: 'Tarea 5', done: false },
+    { id: 6, description: 'Tarea 6', done: false },
+  ];
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number): Todo {
+    return null;
+  }
+
+  createTodo(todo: Todo): Todo {
+    return null;
+  }
+
+  updateTodo(id: number): Todo {
+    return null;
+  }
+
+  deleteTodo(id: number): Todo {
+    return null;
+  }
+}
